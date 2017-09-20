017b66775d808da3ff3764f1edc021964ab0cfbc
Sean Hokanson <sean.hokanson@gmail.com>
2017-09-20 08:59:20 -0600

    _now_ the OR_2 tests are finished

--- MSTest.PetzoldComputer/ORTest.cs
+++ MSTest.PetzoldComputer/ORTest.cs
@@ -194,11 +194,11 @@ namespace MSTest.PetzoldComputer
 		}
 
 		[TestMethod]
-		public void OR_2_Events_On()
+		public void OR_2_Events_GateOn()
 		{
 			// arrage
 			var or = new OR_2();
-			or.V.V = VoltageSignal.HIGH;
+			or.V.V = VoltageSignal.HIGH;	// turn on the gate
 			bool fired = false;
 			or.O.Changed += _ => fired = true;
 
@@ -251,6 +251,115 @@ namespace MSTest.PetzoldComputer
 			Assert.IsFalse(fired, "Gate on -- B: H; A: v; no event");
 			or.A.V = VoltageSignal.LOW;
 			Assert.IsFalse(fired, "Gate on -- B: H; A: -->L; no event");
+		}
+
+		[TestMethod]
+		public void OR_2_Events_GateOff()
+		{
+			// arrange
+			var or = new OR_2();
+			bool fired = false;
+			or.O.Changed += _ => fired = true;
+
+			// act, assert
+			or.B.V = VoltageSignal.HIGH;
+			Assert.IsFalse(fired, "Gate off -- A: L; B: ^; no event");
+			or.B.V = VoltageSignal.HIGH;
+			Assert.IsFalse(fired, "Gate off -- A: L; B: -->H; no event");
+			or.B.V = VoltageSignal.LOW;
+			Assert.IsFalse(fired, "Gate off -- A: L; B: v; no event");
+			or.B.V = VoltageSignal.LOW;
+			Assert.IsFalse(fired, "Gate off -- A: L; B: -->L; no event");
+			or.A.V = VoltageSignal.HIGH;
+			Assert.IsFalse(fired, "Gate off -- B: L; A: ^; no event");
+			or.A.V = VoltageSignal.HIGH;
+			Assert.IsFalse(fired, "Gate off -- B: L; A: -->H; no event");
+			or.A.V = VoltageSignal.LOW;
+			Assert.IsFalse(fired, "Gate off -- B: L; A: v; no event");
+			or.A.V = VoltageSignal.LOW;
+			Assert.IsFalse(fired, "Gate off -- B: L; A: -->L; no event");
+
+			// reset
+			or.A.V = VoltageSignal.HIGH;
+			or.B.V = VoltageSignal.LOW;
+			
+			or.B.V = VoltageSignal.HIGH;
+			Assert.IsFalse(fired, "Gate off -- A: H; B: ^; no event");
+			or.B.V = VoltageSignal.HIGH;
+			Assert.IsFalse(fired, "Gate off -- A: H; B: -->H; no event");
+			or.B.V = VoltageSignal.LOW;
+			Assert.IsFalse(fired, "Gate off -- A: H; B: v; no event");
+			or.B.V = VoltageSignal.LOW;
+			Assert.IsFalse(fired, "Gate off -- A: H; B: -->L; no event");
+
+			// reset
+			or.A.V = VoltageSignal.LOW;
+			or.B.V = VoltageSignal.HIGH;
+
+			or.A.V = VoltageSignal.HIGH;
+			Assert.IsFalse(fired, "Gate off -- B: H; A: ^; no event");
+			or.A.V = VoltageSignal.HIGH;
+			Assert.IsFalse(fired, "Gate off -- B: H; A: -->H; no event");
+			or.A.V = VoltageSignal.LOW;
+			Assert.IsFalse(fired, "Gate off -- B: H; A: v; no event");
+			or.A.V = VoltageSignal.LOW;
+			Assert.IsFalse(fired, "Gate off -- B: H; A: -->L; no event");
+		}
+
+		[TestMethod]
+		public void OR_2_Event_GateOnOff()
+		{
+			// arrange
+			var or = new OR_2();
+			bool fired = false;
+			or.O.Changed += _ => fired = true;
+
+			// act, assert
+			or.V.V = VoltageSignal.HIGH;
+			Assert.IsFalse(fired, "A: L; B: L; V: ^; no event");
+			or.V.V = VoltageSignal.HIGH;
+			Assert.IsFalse(fired, "A: L; B: L; V: --->H; no event");
+			or.V.V = VoltageSignal.LOW;
+			Assert.IsFalse(fired, "A: L; B: L; V: v; no event");
+			or.V.V = VoltageSignal.LOW;
+			Assert.IsFalse(fired, "A: L; B: L; V: --->L; no event");
+
+			or.A.V = VoltageSignal.HIGH;
+			or.V.V = VoltageSignal.HIGH;
+			Assert.IsTrue(fired, "A: H; B: L; V: ^; event");
+			fired = false;
+			or.V.V = VoltageSignal.HIGH;
+			Assert.IsFalse(fired, "A: H; B: L; V: --->H; no event");
+			or.V.V = VoltageSignal.LOW;
+			Assert.IsTrue(fired, "A: H; B: L; V: v; event");
+			fired = false;
+			or.V.V = VoltageSignal.LOW;
+			Assert.IsFalse(fired, "A: H; B: L; V: --->L; no event");
+
+			or.A.V = VoltageSignal.LOW;
+			or.B.V = VoltageSignal.HIGH;
+			or.V.V = VoltageSignal.HIGH;
+			Assert.IsTrue(fired, "A: L; B: H; V: ^; event");
+			fired = false;
+			or.V.V = VoltageSignal.HIGH;
+			Assert.IsFalse(fired, "A: L; B: H; V: --->H; no event");
+			or.V.V = VoltageSignal.LOW;
+			Assert.IsTrue(fired, "A: L; B: H; V: v; event");
+			fired = false;
+			or.V.V = VoltageSignal.LOW;
+			Assert.IsFalse(fired, "A: L; B: H; V: --->L; no event");
+
+			or.A.V = VoltageSignal.HIGH;
+			or.V.V = VoltageSignal.HIGH;
+			Assert.IsTrue(fired, "A: H; B: H; V: ^; event");
+			fired = false;
+			or.V.V = VoltageSignal.HIGH;
+			Assert.IsFalse(fired, "A: H; B: H; V: --->H; no event");
+			or.V.V = VoltageSignal.LOW;
+			Assert.IsTrue(fired, "A: H; B: H; V: v; event");
+			fired = false;
+			or.V.V = VoltageSignal.LOW;
+			Assert.IsFalse(fired, "A: H; B: H; V: --->L; no event");
 		}
 	}
 }
