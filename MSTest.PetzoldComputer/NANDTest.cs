4504bdae4e97ca99ed0b47b51a7cb2ad62098550
Sean Hokanson <sean.hokanson@gmail.com>
2017-09-20 20:46:28 -0600

    NAND_2

--- MSTest.PetzoldComputer/NANDTest.cs
+++ MSTest.PetzoldComputer/NANDTest.cs
@@ -158,5 +158,213 @@ namespace MSTest.PetzoldComputer
 			nand.A = VoltageSignal.LOW;
 			Assert.IsFalse(helper.EventFired, "Gate off -- B: H; A: -->L; no event");
 		}
+
+		[TestMethod]
+		public void NAND_2_Constructor()
+		{
+			var nand = new NAND_2();
+
+			Assert.AreEqual(VoltageSignal.LOW, nand.V.V, "NAND Constructor: Voltage");
+			Assert.AreEqual(VoltageSignal.LOW, nand.A.V, "NAND Constructor: A");
+			Assert.AreEqual(VoltageSignal.LOW, nand.B.V, "NAND Constructor: B");
+			Assert.AreEqual(VoltageSignal.LOW, nand.O.V, "NAND Constructor: O");
+			Assert.AreEqual("LOW", nand.ToString(), "NAND Constructor: ToString()");
+		}
+
+		[DataTestMethod]
+		#region data
+		[DataRow(VoltageSignal.LOW, VoltageSignal.LOW, VoltageSignal.LOW, VoltageSignal.LOW)]
+		[DataRow(VoltageSignal.LOW, VoltageSignal.LOW, VoltageSignal.HIGH, VoltageSignal.LOW)]
+		[DataRow(VoltageSignal.LOW, VoltageSignal.HIGH, VoltageSignal.LOW, VoltageSignal.LOW)]
+		[DataRow(VoltageSignal.LOW, VoltageSignal.HIGH, VoltageSignal.HIGH, VoltageSignal.LOW)]
+		[DataRow(VoltageSignal.HIGH, VoltageSignal.LOW, VoltageSignal.LOW, VoltageSignal.HIGH)]
+		[DataRow(VoltageSignal.HIGH, VoltageSignal.LOW, VoltageSignal.HIGH, VoltageSignal.HIGH)]
+		[DataRow(VoltageSignal.HIGH, VoltageSignal.HIGH, VoltageSignal.LOW, VoltageSignal.HIGH)]
+		[DataRow(VoltageSignal.HIGH, VoltageSignal.HIGH, VoltageSignal.HIGH, VoltageSignal.LOW)]
+		#endregion
+		public void NAND_2(VoltageSignal voltage, VoltageSignal a, VoltageSignal b, VoltageSignal expected)
+		{
+			// arrage
+			var nand = new NAND_2();
+
+			// act
+			nand.V.V = voltage;
+			nand.A.V = a;
+			nand.B.V = b;
+
+			// assert
+			Assert.AreEqual(expected, nand.O.V, $"V:{nand.V.V}; A:{nand.A.V}; B:{nand.B.V}");
+		}
+
+		[TestMethod]
+		public void NAND_2_Events_GateOn()
+		{
+			// arrange
+			var nand = new NAND_2();
+			nand.V.V = VoltageSignal.HIGH;
+			bool fired = false;
+			nand.O.Changed += _ => fired = true;
+
+			// act, assert
+			nand.B.V = VoltageSignal.HIGH;
+			Assert.IsFalse(fired, "Gate on -- A: L; B: ^; no event");
+			nand.B.V = VoltageSignal.HIGH;
+			Assert.IsFalse(fired, "Gate on -- A: L; B: -->H; no event");
+			nand.B.V = VoltageSignal.LOW;
+			Assert.IsFalse(fired, "Gate on -- A: L; B: v; no event");
+			nand.B.V = VoltageSignal.LOW;
+			Assert.IsFalse(fired, "Gate on -- A: L; B: -->L; no event");
+			nand.A.V = VoltageSignal.HIGH;
+			Assert.IsFalse(fired, "Gate on -- B: L; A: ^; no event");
+			nand.A.V = VoltageSignal.HIGH;
+			Assert.IsFalse(fired, "Gate on -- B: L; A: -->H; no event");
+			nand.A.V = VoltageSignal.LOW;
+			Assert.IsFalse(fired, "Gate on -- B: L; A: v; no event");
+			nand.A.V = VoltageSignal.LOW;
+			Assert.IsFalse(fired, "Gate on -- B: L; A: -->L; no event");
+
+			// setup
+			nand.A.V = VoltageSignal.HIGH;
+			nand.B.V = VoltageSignal.LOW;
+			fired = false;
+			// test
+			nand.B.V = VoltageSignal.HIGH;
+			Assert.IsTrue(fired, "Gate on -- A: H; B: ^; event");
+			fired = false;
+			nand.B.V = VoltageSignal.HIGH;
+			Assert.IsFalse(fired, "Gate on -- A: H; B: -->H; no event");
+			nand.B.V = VoltageSignal.LOW;
+			Assert.IsTrue(fired, "Gate on -- A: H; B: v; event");
+			fired = false;
+			nand.B.V = VoltageSignal.LOW;
+			Assert.IsFalse(fired, "Gate on -- A: H; B: -->L; no event");
+
+			// setup
+			nand.A.V = VoltageSignal.LOW;
+			nand.B.V = VoltageSignal.HIGH;
+			fired = false;
+			// test
+			nand.A.V = VoltageSignal.HIGH;
+			Assert.IsTrue(fired, "Gate on -- B: H; A: ^; event");
+			fired = false;
+			nand.A.V = VoltageSignal.HIGH;
+			Assert.IsFalse(fired, "Gate on -- B: H; A: -->H; no event");
+			nand.A.V = VoltageSignal.LOW;
+			Assert.IsTrue(fired, "Gate on -- B: H; A: v; event");
+			fired = false;
+			nand.A.V = VoltageSignal.LOW;
+			Assert.IsFalse(fired, "Gate on -- B: H; A: -->L; no event");
+		}
+
+		[TestMethod]
+		public void AND_2_Events_GateOff()
+		{
+			// arrange
+			var nand = new NAND_2();
+			bool fired = false;
+			nand.O.Changed += _ => fired = true;
+
+			// act, assert
+			nand.B.V = VoltageSignal.HIGH;
+			Assert.IsFalse(fired, "Gate off -- A: L; B: ^; no event");
+			nand.B.V = VoltageSignal.HIGH;
+			Assert.IsFalse(fired, "Gate off -- A: L; B: -->H; no event");
+			nand.B.V = VoltageSignal.LOW;
+			Assert.IsFalse(fired, "Gate off -- A: L; B: v; no event");
+			nand.B.V = VoltageSignal.LOW;
+			Assert.IsFalse(fired, "Gate off -- A: L; B: -->L; no event");
+			nand.A.V = VoltageSignal.HIGH;
+			Assert.IsFalse(fired, "Gate off -- B: L; A: ^; no event");
+			nand.A.V = VoltageSignal.HIGH;
+			Assert.IsFalse(fired, "Gate off -- B: L; A: -->H; no event");
+			nand.A.V = VoltageSignal.LOW;
+			Assert.IsFalse(fired, "Gate off -- B: L; A: v; no event");
+			nand.A.V = VoltageSignal.LOW;
+			Assert.IsFalse(fired, "Gate off -- B: L; A: -->L; no event");
+
+			// reset
+			nand.A.V = VoltageSignal.HIGH;
+			nand.B.V = VoltageSignal.LOW;
+
+			nand.B.V = VoltageSignal.HIGH;
+			Assert.IsFalse(fired, "Gate off -- A: H; B: ^; no event");
+			nand.B.V = VoltageSignal.HIGH;
+			Assert.IsFalse(fired, "Gate off -- A: H; B: -->H; no event");
+			nand.B.V = VoltageSignal.LOW;
+			Assert.IsFalse(fired, "Gate off -- A: H; B: v; no event");
+			nand.B.V = VoltageSignal.LOW;
+			Assert.IsFalse(fired, "Gate off -- A: H; B: -->L; no event");
+
+			// reset
+			nand.A.V = VoltageSignal.LOW;
+			nand.B.V = VoltageSignal.HIGH;
+
+			nand.A.V = VoltageSignal.HIGH;
+			Assert.IsFalse(fired, "Gate off -- B: H; A: ^; no event");
+			nand.A.V = VoltageSignal.HIGH;
+			Assert.IsFalse(fired, "Gate off -- B: H; A: -->H; no event");
+			nand.A.V = VoltageSignal.LOW;
+			Assert.IsFalse(fired, "Gate off -- B: H; A: v; no event");
+			nand.A.V = VoltageSignal.LOW;
+			Assert.IsFalse(fired, "Gate off -- B: H; A: -->L; no event");
+		}
+
+		[TestMethod]
+		public void NAND_2_Event_GateOnOff()
+		{
+			// arrange
+			var nand = new NAND_2();
+			bool fired = false;
+			nand.O.Changed += _ => fired = true;
+
+			// act, assert
+			nand.V.V = VoltageSignal.HIGH;
+			Assert.IsTrue(fired, "A: L; B: L; V: ^; event");
+			fired = false;
+			nand.V.V = VoltageSignal.HIGH;
+			Assert.IsFalse(fired, "A: L; B: L; V: --->H; no event");
+			nand.V.V = VoltageSignal.LOW;
+			Assert.IsTrue(fired, "A: L; B: L; V: v; event");
+			fired = false;
+			nand.V.V = VoltageSignal.LOW;
+			Assert.IsFalse(fired, "A: L; B: L; V: --->L; no event");
+
+			nand.A.V = VoltageSignal.HIGH;
+			fired = false;
+			nand.V.V = VoltageSignal.HIGH;
+			Assert.IsTrue(fired, "A: H; B: L; V: ^; event");
+			fired = false;
+			nand.V.V = VoltageSignal.HIGH;
+			Assert.IsFalse(fired, "A: H; B: L; V: --->H; no event");
+			nand.V.V = VoltageSignal.LOW;
+			Assert.IsTrue(fired, "A: H; B: L; V: v; event");
+			fired = false;
+			nand.V.V = VoltageSignal.LOW;
+			Assert.IsFalse(fired, "A: H; B: L; V: --->L; no event");
+
+			nand.A.V = VoltageSignal.LOW;
+			nand.B.V = VoltageSignal.HIGH;
+			fired = false;
+			nand.V.V = VoltageSignal.HIGH;
+			Assert.IsTrue(fired, "A: L; B: H; V: ^; event");
+			fired = false;
+			nand.V.V = VoltageSignal.HIGH;
+			Assert.IsFalse(fired, "A: L; B: H; V: --->H; no event");
+			nand.V.V = VoltageSignal.LOW;
+			Assert.IsTrue(fired, "A: L; B: H; V: v; event");
+			fired = false;
+			nand.V.V = VoltageSignal.LOW;
+			Assert.IsFalse(fired, "A: L; B: H; V: --->L; no event");
+
+			nand.A.V = VoltageSignal.HIGH;
+			nand.V.V = VoltageSignal.HIGH;
+			Assert.IsFalse(fired, "A: H; B: H; V: ^; no event");
+			nand.V.V = VoltageSignal.HIGH;
+			Assert.IsFalse(fired, "A: H; B: H; V: --->H; no event");
+			nand.V.V = VoltageSignal.LOW;
+			Assert.IsFalse(fired, "A: H; B: H; V: v; no event");
+			nand.V.V = VoltageSignal.LOW;
+			Assert.IsFalse(fired, "A: H; B: H; V: --->L; no event");
+		}
 	}
 }
--- NAND.cs
+++ NAND.cs
@@ -40,7 +40,8 @@ namespace PetzoldComputer
 		public VoltageSignal A
 		{
 			get => _relay1.Input;
-			set {
+			set
+			{
 				VoltageSignal oldOutput = _output;
 
 				_relay1.Input = value;
@@ -52,7 +53,8 @@ namespace PetzoldComputer
 		public VoltageSignal B
 		{
 			get => _relay2.Input;
-			set {
+			set
+			{
 				VoltageSignal oldOutput = _output;
 
 				_relay2.Input = value;
@@ -89,5 +91,41 @@ namespace PetzoldComputer
 			}
 		}
 		#endregion
+	}
+
+	public class NAND_2
+	{
+		public NAND_2()
+		{
+			_not1 = new NOT_2();
+			_not2 = new NOT_2();
+			_output = new ConnectionPoint();
+
+			_not1.Voltage.ConnectTo(_not2.Voltage);
+			// wiring it up this way doesn't really make sense
+			//_not1.Output.ConnectTo(_output);
+			//_not2.Output.ConnectTo(_output);
+			// ...so we'll do some manual wiring
+			_not1.Output.Changed += OnNotOutputChanged;
+			_not2.Output.Changed += OnNotOutputChanged;
+		}
+
+		private readonly NOT_2 _not1;
+		private readonly NOT_2 _not2;
+		private readonly ConnectionPoint _output;
+
+		public ConnectionPoint V => _not1.Voltage;
+		public ConnectionPoint A => _not1.Input;
+		public ConnectionPoint B => _not2.Input;
+		public ConnectionPoint O => _output;
+
+		public override string ToString() => $"{O}";
+
+		private void OnNotOutputChanged(ConnectionPoint cp)
+		{
+			_output.V = _not1.Output.V == VoltageSignal.HIGH || _not2.Output.V == VoltageSignal.HIGH
+				? VoltageSignal.HIGH
+				: VoltageSignal.LOW;
+		}
 	}
 }
