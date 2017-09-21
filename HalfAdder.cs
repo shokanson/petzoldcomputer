e7d5b4fe5d79f3c951cb3cc8fd2d558e7b35c0b6
Sean Hokanson <sean.hokanson@gmail.com>
2017-09-21 07:17:21 -0600

    HalfAdder_2

--- HalfAdder.cs
+++ HalfAdder.cs
@@ -58,4 +58,28 @@ namespace PetzoldComputer
 		public override string ToString() => $"Sum: {_sumXor}; Carry: {_carryAnd}";
 		#endregion
 	}
+
+	public class HalfAdder_2
+	{
+		public HalfAdder_2()
+		{
+			_xor = new XOR_2();
+			_and = new AND_2();
+
+			_xor.V.ConnectTo(_and.V);
+			_xor.A.ConnectTo(_and.A);
+			_xor.B.ConnectTo(_and.B);
+		}
+
+		private readonly XOR_2 _xor;
+		private readonly AND_2 _and;
+
+		public ConnectionPoint V => _xor.V;
+		public ConnectionPoint A => _xor.A;
+		public ConnectionPoint B => _xor.B;
+		public ConnectionPoint Sum => _xor.O;
+		public ConnectionPoint Carry => _and.O;
+
+		public override string ToString() => $"Sum: {Sum}; Carry: {Carry}";
+	}
 }
--- MSTest.PetzoldComputer/HalfAdderTest.cs
+++ MSTest.PetzoldComputer/HalfAdderTest.cs
@@ -169,5 +169,192 @@ namespace MSTest.PetzoldComputer
 			halfAdder.A = VoltageSignal.LOW;
 			Assert.IsFalse(helper.EventFired, "Gate on -- B: H; A: -->L; no event");
 		}
+
+		[TestMethod]
+		public void HalfAdder_2_Constructor()
+		{
+			var halfAdder = new HalfAdder_2();
+
+			Assert.AreEqual(VoltageSignal.LOW, halfAdder.V.V, "Constructor: V");
+			Assert.AreEqual(VoltageSignal.LOW, halfAdder.A.V, "Constructor: A");
+			Assert.AreEqual(VoltageSignal.LOW, halfAdder.B.V, "Constructor: B");
+			Assert.AreEqual(VoltageSignal.LOW, halfAdder.Sum.V, "Constructor: Sum");
+			Assert.AreEqual(VoltageSignal.LOW, halfAdder.Carry.V, "Constructor: Carry");
+			Assert.AreEqual("Sum: LOW; Carry: LOW", halfAdder.ToString(), "Constructor: ToString()");
+		}
+
+		[DataTestMethod]
+		#region data
+		[DataRow(VoltageSignal.LOW, VoltageSignal.LOW, VoltageSignal.LOW, VoltageSignal.LOW)]
+		[DataRow(VoltageSignal.LOW, VoltageSignal.LOW, VoltageSignal.HIGH, VoltageSignal.LOW)]
+		[DataRow(VoltageSignal.LOW, VoltageSignal.HIGH, VoltageSignal.LOW, VoltageSignal.LOW)]
+		[DataRow(VoltageSignal.LOW, VoltageSignal.HIGH, VoltageSignal.HIGH, VoltageSignal.LOW)]
+		[DataRow(VoltageSignal.HIGH, VoltageSignal.LOW, VoltageSignal.LOW, VoltageSignal.LOW)]
+		[DataRow(VoltageSignal.HIGH, VoltageSignal.LOW, VoltageSignal.HIGH, VoltageSignal.HIGH)]
+		[DataRow(VoltageSignal.HIGH, VoltageSignal.HIGH, VoltageSignal.LOW, VoltageSignal.HIGH)]
+		[DataRow(VoltageSignal.HIGH, VoltageSignal.HIGH, VoltageSignal.HIGH, VoltageSignal.LOW)]
+		#endregion
+		public void XOR_2_Sum(VoltageSignal voltage, VoltageSignal a, VoltageSignal b, VoltageSignal expected)
+		{
+			// arrage
+			var halfAdder = new HalfAdder_2();
+
+			// act
+			halfAdder.V.V = voltage;
+			halfAdder.A.V = a;
+			halfAdder.B.V = b;
+
+			// assert
+			Assert.AreEqual(expected, halfAdder.Sum.V, $"V:{halfAdder.V.V}; A:{halfAdder.A.V}; B:{halfAdder.B.V}");
+		}
+
+		[DataTestMethod]
+		#region data
+		[DataRow(VoltageSignal.LOW, VoltageSignal.LOW, VoltageSignal.LOW, VoltageSignal.LOW)]
+		[DataRow(VoltageSignal.LOW, VoltageSignal.LOW, VoltageSignal.HIGH, VoltageSignal.LOW)]
+		[DataRow(VoltageSignal.LOW, VoltageSignal.HIGH, VoltageSignal.LOW, VoltageSignal.LOW)]
+		[DataRow(VoltageSignal.LOW, VoltageSignal.HIGH, VoltageSignal.HIGH, VoltageSignal.LOW)]
+		[DataRow(VoltageSignal.HIGH, VoltageSignal.LOW, VoltageSignal.LOW, VoltageSignal.LOW)]
+		[DataRow(VoltageSignal.HIGH, VoltageSignal.LOW, VoltageSignal.HIGH, VoltageSignal.LOW)]
+		[DataRow(VoltageSignal.HIGH, VoltageSignal.HIGH, VoltageSignal.LOW, VoltageSignal.LOW)]
+		[DataRow(VoltageSignal.HIGH, VoltageSignal.HIGH, VoltageSignal.HIGH, VoltageSignal.HIGH)]
+		#endregion
+		public void HalfAdder_2_Carry(VoltageSignal voltage, VoltageSignal a, VoltageSignal b, VoltageSignal expected)
+		{
+			// arrage
+			var halfAdder = new HalfAdder_2();
+
+			// act
+			halfAdder.V.V = voltage;
+			halfAdder.A.V = a;
+			halfAdder.B.V = b;
+
+			// assert
+			Assert.AreEqual(expected, halfAdder.Carry.V, $"V:{halfAdder.V.V}; A:{halfAdder.A.V}; B:{halfAdder.B.V}");
+		}
+
+		[TestMethod]
+		public void HalfAdder_2_Sum_Event()
+		{
+			// arrange
+			var halfAdder = new HalfAdder_2();
+			halfAdder.V.V = VoltageSignal.HIGH;
+			bool fired = false;
+			halfAdder.Sum.Changed += _ => fired = true;
+
+			// act, assert
+			halfAdder.B.V = VoltageSignal.HIGH;
+			Assert.IsTrue(fired, "Gate on -- A: L; B: ^; event");
+			fired = false;
+			halfAdder.B.V = VoltageSignal.HIGH;
+			Assert.IsFalse(fired, "Gate on -- A: L; B: -->H; no event");
+			halfAdder.B.V = VoltageSignal.LOW;
+			Assert.IsTrue(fired, "Gate on -- A: L; B: v; event");
+			fired = false;
+			halfAdder.B.V = VoltageSignal.LOW;
+			Assert.IsFalse(fired, "Gate on -- A: L; B: -->L; no event");
+			halfAdder.A.V = VoltageSignal.HIGH;
+			Assert.IsTrue(fired, "Gate on -- B: L; A: ^; event");
+			fired = false;
+			halfAdder.A.V = VoltageSignal.HIGH;
+			Assert.IsFalse(fired, "Gate on -- B: L; A: -->H; no event");
+			halfAdder.A.V = VoltageSignal.LOW;
+			Assert.IsTrue(fired, "Gate on -- B: L; A: v; event");
+			fired = false;
+			halfAdder.A.V = VoltageSignal.LOW;
+			Assert.IsFalse(fired, "Gate on -- B: L; A: -->L; no event");
+
+			// setup
+			halfAdder.A.V = VoltageSignal.HIGH;
+			halfAdder.B.V = VoltageSignal.LOW;
+			fired = false;
+			// test
+			halfAdder.B.V = VoltageSignal.HIGH;
+			Assert.IsTrue(fired, "Gate on -- A: H; B: ^; event");
+			fired = false;
+			halfAdder.B.V = VoltageSignal.HIGH;
+			Assert.IsFalse(fired, "Gate on -- A: H; B: -->H; no event");
+			halfAdder.B.V = VoltageSignal.LOW;
+			Assert.IsTrue(fired, "Gate on -- A: H; B: v; event");
+			fired = false;
+			halfAdder.B.V = VoltageSignal.LOW;
+			Assert.IsFalse(fired, "Gate on -- A: H; B: -->L; no event");
+
+			// setup
+			halfAdder.A.V = VoltageSignal.LOW;
+			halfAdder.B.V = VoltageSignal.HIGH;
+			fired = false;
+			// test
+			halfAdder.A.V = VoltageSignal.HIGH;
+			Assert.IsTrue(fired, "Gate on -- B: H; A: ^; event");
+			fired = false;
+			halfAdder.A.V = VoltageSignal.HIGH;
+			Assert.IsFalse(fired, "Gate on -- B: H; A: -->H; no event");
+			halfAdder.A.V = VoltageSignal.LOW;
+			Assert.IsTrue(fired, "Gate on -- B: H; A: v; event");
+			fired = false;
+			halfAdder.A.V = VoltageSignal.LOW;
+			Assert.IsFalse(fired, "Gate on -- B: H; A: -->L; no event");
+		}
+
+		[TestMethod]
+		public void HalfAdder_2_Carry_Event()
+		{
+			// arrange
+			var halfAdder = new HalfAdder_2();
+			halfAdder.V.V = VoltageSignal.HIGH;
+			bool fired = false;
+			halfAdder.Carry.Changed += _ => fired = true;
+
+			// act, assert
+			halfAdder.B.V = VoltageSignal.HIGH;
+			Assert.IsFalse(fired, "Gate on -- A: L; B: ^; no event");
+			halfAdder.B.V = VoltageSignal.HIGH;
+			Assert.IsFalse(fired, "Gate on -- A: L; B: -->H; no event");
+			halfAdder.B.V = VoltageSignal.LOW;
+			Assert.IsFalse(fired, "Gate on -- A: L; B: v; no event");
+			halfAdder.B.V = VoltageSignal.LOW;
+			Assert.IsFalse(fired, "Gate on -- A: L; B: -->L; no event");
+			halfAdder.A.V = VoltageSignal.HIGH;
+			Assert.IsFalse(fired, "Gate on -- B: L; A: ^; no event");
+			halfAdder.A.V = VoltageSignal.HIGH;
+			Assert.IsFalse(fired, "Gate on -- B: L; A: -->H; no event");
+			halfAdder.A.V = VoltageSignal.LOW;
+			Assert.IsFalse(fired, "Gate on -- B: L; A: v; no event");
+			halfAdder.A.V = VoltageSignal.LOW;
+			Assert.IsFalse(fired, "Gate on -- B: L; A: -->L; no event");
+
+			// setup
+			halfAdder.A.V = VoltageSignal.HIGH;
+			halfAdder.B.V = VoltageSignal.LOW;
+			fired = false;
+			// test
+			halfAdder.B.V = VoltageSignal.HIGH;
+			Assert.IsTrue(fired, "Gate on -- A: H; B: ^; event");
+			fired = false;
+			halfAdder.B.V = VoltageSignal.HIGH;
+			Assert.IsFalse(fired, "Gate on -- A: H; B: -->H; no event");
+			halfAdder.B.V = VoltageSignal.LOW;
+			Assert.IsTrue(fired, "Gate on -- A: H; B: v; event");
+			fired = false;
+			halfAdder.B.V = VoltageSignal.LOW;
+			Assert.IsFalse(fired, "Gate on -- A: H; B: -->L; no event");
+
+			// setup
+			halfAdder.A.V = VoltageSignal.LOW;
+			halfAdder.B.V = VoltageSignal.HIGH;
+			fired = false;
+			// test
+			halfAdder.A.V = VoltageSignal.HIGH;
+			Assert.IsTrue(fired, "Gate on -- B: H; A: ^; event");
+			fired = false;
+			halfAdder.A.V = VoltageSignal.HIGH;
+			Assert.IsFalse(fired, "Gate on -- B: H; A: -->H; no event");
+			halfAdder.A.V = VoltageSignal.LOW;
+			Assert.IsTrue(fired, "Gate on -- B: H; A: v; event");
+			fired = false;
+			halfAdder.A.V = VoltageSignal.LOW;
+			Assert.IsFalse(fired, "Gate on -- B: H; A: -->L; no event");
+		}
 	}
 }
