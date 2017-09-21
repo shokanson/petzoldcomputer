2c86aebf94a88eb4f1853da6694ad764e0c4a96e
Sean Hokanson <sean.hokanson@gmail.com>
2017-09-20 21:49:44 -0600

    NOR3_2

--- NOR3.cs
+++ NOR3.cs
@@ -73,4 +73,29 @@ namespace PetzoldComputer
 		}
 		#endregion
 	}
+
+	public class NOR3_2
+	{
+		public NOR3_2()
+		{
+			_not1 = new NOT_2();
+			_not2 = new NOT_2();
+			_not3 = new NOT_2();
+
+			_not1.Output.ConnectTo(_not2.Voltage);
+			_not2.Output.ConnectTo(_not3.Voltage);
+		}
+
+		private readonly NOT_2 _not1;
+		private readonly NOT_2 _not2;
+		private readonly NOT_2 _not3;
+
+		public ConnectionPoint V => _not1.Voltage;
+		public ConnectionPoint A => _not1.Input;
+		public ConnectionPoint B => _not2.Input;
+		public ConnectionPoint C => _not3.Input;
+		public ConnectionPoint O => _not3.Output;
+
+		public override string ToString() => $"{O}";
+	}
 }
